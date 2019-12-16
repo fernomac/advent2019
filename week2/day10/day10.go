4ac7b84fa12b4ab37f672c731ca9e34a1f5ad096
David Murray <fernomac@coronet.io>
2019-12-15 23:51:44 -0800

    day 10

---
+++ week2/day10/day10.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+	"strings"
+
+	"github.com/fernomac/advent2019/lib"
+)
+
+type point struct {
+	x, y int
+}
+
+type try struct {
+	roids   map[point]bool
+	size    point
+	station point
+}
+
+func (t *try) stepFrom(x, y int) (int, int) {
+	dx := t.station.x - x
+	dy := t.station.y - y
+
+	div := gcd(dx, dy)
+	if div == 0 {
+		return 0, 0
+	}
+
+	return dx / div, dy / div
+}
+
+func (t *try) trace(x, y int) bool {
+	ret := false
+
+	pt := point{x, y}
+	dx, dy := t.stepFrom(x, y)
+
+	for pt.x != t.station.x || pt.y != t.station.y {
+		if t.roids[pt] {
+			ret = true
+			delete(t.roids, pt)
+		}
+
+		pt.x += dx
+		pt.y += dy
+	}
+
+	return ret
+}
+
+func (t *try) traceline(y int) int {
+	count := 0
+	for x := 0; x < t.size.x; x++ {
+		if t.trace(x, y) {
+			count++
+		}
+	}
+	return count
+}
+
+func (t *try) count() int {
+	count := 0
+
+	for y := 0; y < t.station.y; y++ {
+		count += t.traceline(y)
+	}
+	for y := t.size.y - 1; y > t.station.y; y-- {
+		count += t.traceline(y)
+	}
+
+	if t.trace(0, t.station.y) {
+		count++
+	}
+	if t.trace(t.size.x-1, t.station.y) {
+		count++
+	}
+
+	return count
+}
+
+type space struct {
+	roids map[point]bool
+	size  point
+}
+
+func parse(str string) space {
+	lines := strings.Split(strings.TrimSpace(str), "\n")
+
+	roids := map[point]bool{}
+	size := point{len(lines[0]), len(lines)}
+
+	for y, line := range lines {
+		for x, char := range line {
+			switch char {
+			case '#':
+				roids[point{x, y}] = true
+			}
+		}
+	}
+
+	return space{roids, size}
+}
+
+func (s *space) try(x, y int) int {
+	roids := map[point]bool{}
+	for pt := range s.roids {
+		roids[pt] = true
+	}
+
+	t := try{
+		roids:   roids,
+		size:    s.size,
+		station: point{x, y},
+	}
+
+	return t.count()
+}
+
+func (s *space) optimize() (int, point) {
+	max := -1
+	maxpt := point{}
+
+	for pt := range s.roids {
+		val := s.try(pt.x, pt.y)
+		if val > max {
+			max = val
+			maxpt = pt
+		}
+	}
+
+	return max, maxpt
+}
+
+type target struct {
+	point    point
+	angle    float64
+	distance int
+}
+
+func (s *space) targets(sx, sy int) []target {
+	ret := []target{}
+
+	for pt := range s.roids {
+		dx := pt.x - sx
+		dy := sy - pt.y
+
+		angle := math.Atan2(float64(dx), float64(dy))
+		if angle < 0 {
+			angle += (math.Pi * 2)
+		}
+
+		ret = append(ret, target{pt, angle, dx + dy})
+	}
+
+	return ret
+}
+
+func (s *space) asplode(n int, targets []target) target {
+	sort.Slice(targets, func(i, j int) bool {
+		return targets[i].angle < targets[j].angle
+	})
+
+	i := 0
+	q := []target{}
+
+	for {
+		t := targets[0]
+		i++
+
+		fmt.Printf("%v: asploding %v\n", i, t.point)
+		if i == n {
+			return t
+		}
+
+		j := 1
+		for ; targets[j].angle == t.angle; j++ {
+			// Keep going.
+		}
+
+		if j > 1 {
+			q = append(q, targets[1:j]...)
+		}
+		targets = targets[j:]
+
+		if len(targets) == 0 {
+			targets = q
+			q = []target{}
+		}
+	}
+}
+
+func main() {
+	space := parse(lib.Load("input.txt"))
+	score, where := space.optimize()
+	fmt.Println(score, ":", where)
+
+	targets := space.targets(where.x, where.y)
+	space.asplode(200, targets)
+}
+
+func gcd(a, b int) int {
+	if a < 0 {
+		a = -a
+	}
+	if b < 0 {
+		b = -b
+	}
+	return _gcd(a, b)
+}
+
+func _gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return _gcd(b, a%b)
+}
---
+++ week2/day10/day10_test.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStepFrom(t *testing.T) {
+	try := try{
+		size:    point{100, 100},
+		station: point{4, 4},
+	}
+	test := func(x, y, edx, edy int) {
+		t.Run(fmt.Sprintf("(%v,%v)", x, y), func(t *testing.T) {
+			dx, dy := try.stepFrom(x, y)
+			if dx != edx {
+				t.Errorf("expected dx=%v, got %v", edx, dx)
+			}
+			if dy != edy {
+				t.Errorf("expected dy=%v, got %v", edy, dy)
+			}
+		})
+	}
+
+	test(0, 0, 1, 1)
+	test(1, 0, 3, 4)
+	test(2, 0, 1, 2)
+	test(3, 0, 1, 4)
+	test(4, 0, 0, 1)
+	test(5, 0, -1, 4)
+	test(6, 0, -1, 2)
+	test(7, 0, -3, 4)
+	test(8, 0, -1, 1)
+}
+
+func TestTrace(t *testing.T) {
+	try := try{
+		roids: map[point]bool{
+			point{1, 0}: true,
+			point{2, 2}: true,
+			point{3, 4}: true,
+			point{4, 4}: true,
+		},
+		size:    point{5, 5},
+		station: point{3, 4},
+	}
+
+	if val := try.trace(1, 0); !val {
+		t.Error("expected trace(1,0)=true")
+	}
+	if val := try.trace(2, 2); val {
+		t.Error("expected trace(2,2)=false")
+	}
+	if val := try.trace(3, 4); val {
+		t.Error("expected trace(3,4)=false")
+	}
+	if val := try.trace(4, 4); !val {
+		t.Error("expected trace(4,4)=true")
+	}
+}
+
+func TestTry(t *testing.T) {
+	test := func(str string, x, y int, ecount int) {
+		t.Run(fmt.Sprintf("%v:(%v,%v)", str, x, y), func(t *testing.T) {
+			space := parse(str)
+			count := space.try(x, y)
+			if count != ecount {
+				t.Errorf("expected %v, got %v", ecount, count)
+			}
+		})
+	}
+
+	test(`
+.#..#
+.....
+#####
+....#
+...##`, 3, 4, 8)
+
+	test(`
+......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####`, 5, 8, 33)
+
+	test(`
+#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###.`, 1, 2, 35)
+
+	test(`
+.#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#..`, 6, 3, 41)
+
+	test(`
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##`, 11, 13, 210)
+}
+
+func TestOptimize(t *testing.T) {
+	test := func(str string, ecount int, ewhere point) {
+		t.Run(str, func(t *testing.T) {
+			space := parse(str)
+			count, where := space.optimize()
+			if count != ecount {
+				t.Errorf("expected %v, got %v", ecount, count)
+			}
+			if where != ewhere {
+				t.Errorf("expected %v, got %v", ewhere, where)
+			}
+		})
+	}
+
+	test(`
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##`, 210, point{11, 13})
+}
+
+func TestGCD(t *testing.T) {
+	if val := gcd(15, 10); val != 5 {
+		t.Errorf("gcd(15,10)==%v", val)
+	}
+
+	if val := gcd(10, 15); val != 5 {
+		t.Errorf("gcd(10,15)==%v", val)
+	}
+
+	if val := gcd(-10, 15); val != 5 {
+		t.Errorf("gcd(-10,15)==%v", val)
+	}
+
+	if val := gcd(10, -15); val != 5 {
+		t.Errorf("gcd(10,-15)==%v", val)
+	}
+
+	if val := gcd(-10, -15); val != 5 {
+		t.Errorf("gcd(-10,-15)==%v", val)
+	}
+}
